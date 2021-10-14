c6c7a0a79cd0c4b8f4f52ac3a7a9ac2403754c16
Oblivion Plain <45317380+oblivionplain@users.noreply.github.com>
2021-10-14 10:24:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function inBetween(string, start, end) {
     return string.slice(string.indexOf(start) + 1).split(end)[0];
-};
+}
 
 function testString(string) {
     return string.startsWith("'") && string.endsWith("'");
-};
+}
 
 function cleanUp(string, ugly) {
-    for (let i = 0; i < string.length; i ++) string[i] = string[i].replace(ugly, `\\${ugly}`);
+    for (let i = 0; i < string.length; i++)
+        string[i] = string[i].replace(ugly, `\\${ugly}`);
     return string;
 }
 
@@ -23,16 +24,46 @@ const handlers = {
         const name = args.shift();
         args = args.join(" ");
         args = args === "last" ? context.lastValue : cleanUp(args, "'");
-        return `let ${name} = ${type !== "int" ? "'" : "+"}${args}${type !== "int" ? "'" : ""};`;
+        context.variables.push({
+            name: name,
+            type: type
+        });
+        return {
+            text: `let ${name} = ${type !== "int" ? "'" : "+"}${args}${type !== "int" ? "'" : ""};`,
+            context
+        };
     },
     function: function(context, args) {
+        const full = args.join(" ");
         const name = args.shift();
         args = args.join(" ");
-        let arguments = inBetween(args, "(", ")");
+        const arguments = inBetween(args, "(", ")");
         args = args.replace(`(${arguments})`, "").split(" ");
         let output = `function ${name}(`;
-        arguments = arguments.split(", ");
-        console.log(arguments)
+        output += arguments.split(", ").map((entry) => entry.split(" ").pop()).join(", ");
+        output += `) { return ${full.split("=>").pop()} };`.trim();
+        context.variables.push({
+            name: name,
+            type: "function"
+        });
+        return {
+            text: output,
+            context
+        };
+    },
+    default: function(context, line) {
+        let text = "";
+        const variable = context.variables.find((instance) =>
+            line.startsWith(instance.name)
+        );
+        if (variable && variable.type === "function") {
+            let args = inBetween(line, "(", ")");
+            text = `${variable.name}(${args});`;
+        }
+        return {
+            text: text,
+            context
+        };
     }
 };
 
@@ -40,14 +71,23 @@ function getJSFromLine(context, line) {
     let args = line.split(" ");
     const action = args.shift();
     if (handlers[action]) return handlers[action](context, args);
-    return handlers.default(context, args);
+    return handlers.default(context, line);
 }
 
 function compile(code) {
+    let context = {
+        code: "",
+        lastValue: null,
+        variables: []
+    };
     let compiled = "";
     code = code.split("\n");
-    code.forEach(line => compiled += getJSFromLine(null, line));
-    console.log("uau" + compiled);
+    code.forEach((line) => {
+        const result = getJSFromLine(context, line);
+        compiled += result.text;
+        context = result.context;
+    });
+    console.log(compiled);
     return eval(compiled);
 }
-require("fs").promises.readFile("main.ob").then(buff => console.log(compile(buff.toString())));
+require("fs").promises.readFile("main.ob").then((buff) => console.log(compile(buff.toString())));
